8654feea46be8e17929a30fc270126301db84a54
SaoriMik <152349587+SaoriMik@users.noreply.github.com>
2023-11-29 12:58:48 -0300

    Add files via upload

---
+++ scripts/PaginaHome/PaginaHome.js
@@ -0,0 +1,11 @@
+function navegaJogos(){
+    window.location.href="./PaginaGames/Games.html"
+}
+function navegaAnimes() {
+    window.location.href="./PaginaAnimes/Animes.html"
+    
+}
+
+function navegaFeS() {
+    window.location.href="./PaginaF&S/F&S.html"
+}
